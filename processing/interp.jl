03a0f1975a7a9f4fb6502c8201e82f4287abcbc5
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-07-11 13:40:06 -0500

    interpolation IDW

---
+++ processing/interp.jl
@@ -0,0 +1,22 @@
+using GeometricalPredicates
+function interp(TR, particle::Particle, VF::VectorFieldsADCIRC, VFvar)
+  threshold = 0.001
+
+  d1 = [particle.lon[end], particle.lat[end]]-[getx(geta(TR[1])),getx(geta(TR[1]))]
+  d2 = [particle.lon[end], particle.lat[end]]-[getx(getb(TR[1])),getx(getb(TR[1]))]
+  d3 = [particle.lon[end], particle.lat[end]]-[getx(getc(TR[1])),getx(getc(TR[1]))]
+
+  if d1[1] < threshold && d1[2] < threshold
+    interp = VFVar[VF.ELE[TR[2],1]]
+  elseif d2[1]< threshold && d2[2] < threshold
+    interp = VFvar[VF.ELE[TR[2],2]]
+  elseif d3[1]< threshold && d3[2] < threshold
+    interp = VFvar[VF.ELE[TR[2],3]]
+  else
+    println(size(VFvar), "  ", size(VF.ELE), "  ", size(VF.U), "   ", size(TR))
+    interp = sum((VFvar[VF.ELE[TR[2],1]]/d1)
+    + (VFvar[VF.ELE[TR[2],2]]/d2) + (VFvar[VF.ELE[TR[2],3]]/d3))/sum((1/d1) + (1/d2) + (1/d3))
+  end
+
+
+end
