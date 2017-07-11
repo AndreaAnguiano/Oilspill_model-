84fa25fbe8dd12ca84183d73d2447c4cc6899940
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-07-11 14:01:52 -0500

    Adjacent triangle

--- tools/Ady.jl
+++ tools/Ady.jl
@@ -1,20 +1,21 @@
+
 function CreateAdy(ELE::Array{Int64,2})
   Ady = ones(length(ELE),3)
-   for i = 1:length(ELE)
-     found = 0
-     j = 1
-     while found < 3
-       if i != j
-        a = ELE[j,1] in ELE[i,1]
-        b = ELE[j,2] in ELE[i,2]
-        c = ELE[j,3] in ELE[i,3]
-        if a + b + c == 2
-          Ady[i, found] = j
-          found += 1
-
+  for i = 1:length(ELE[:,1])
+      j = 1
+      found = 1
+      while found < 3 & j > length(ELE[:,1])
+        if i != j
+          a = ELE[i,1] in ELE[j,1]
+          b = ELE[i,2] in ELE[j,2]
+          c = ELE[i,3] in ELE[j,3]
+          if a + b + c == 2
+            Ady[i, found] = j
+            found += 1
+          end
         end
-       end
-     end
-   end
-   return Ady
-end
+      end
+      j +=1
+  end
+  return Ady
+ end
