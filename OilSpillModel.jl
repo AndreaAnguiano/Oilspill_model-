8113dc23ce73da046e58d58eae3a71fc0fd7b2f3
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-07-12 12:53:56 -0500

    Iteration over Datetime objects

--- OilSpillModel.jl
+++ OilSpillModel.jl
@@ -1,27 +1,24 @@
-using Plots
-pyplot()
-function oilSpillModel(modelConfigs, FileName, ArrVF3, ArrVF2, ArrVF1,ArrIntVF2,ArrIntVF1, ArrTR, startJulianDate, endJulianDate, visualize, deltaT, lat, lon)
+
+function oilSpillModel(modelConfigs, FileName, ArrVF3, ArrVF2, ArrVF1,ArrIntVF2,ArrIntVF1, ArrTR, startDate, endDate, visualize, deltaT, lat, lon)
   #Start of all fields with the initial conditions
-  spillData = readData(FileName) #Reading all the values of the spill
-  VF = vectorFields(ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2, startJulianDate,0, lat, lon, ArrVF1,ArrVF1,ArrVF2,ArrVF2) #Initial vectorfield
-  VFADCIRC = VectorFieldsADCIRC(ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,startJulianDate,0,lat,lon, ArrVF1, ArrVF1, ArrVF2, "atmFilePrefix", "oceanFilePrefix", "uvar", "vvar", ArrIntVF2,ArrIntVF2,ArrIntVF2, ArrVF2, ArrVF2,ArrTR,ArrVF2 )
+  spillData = OilSpillData(FileName) #Reading all the values of the spill
+  VF = vectorFields(ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2, toJulianDate(startDate),0, lat, lon, ArrVF1,ArrVF1,ArrVF2,ArrVF2) #Initial vectorfield
+  VFADCIRC = VectorFieldsADCIRC(ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,toJulianDate(startDate),0,lat,lon, ArrVF1, ArrVF1, ArrVF2, "atmFilePrefix", "oceanFilePrefix", "uvar", "vvar", ArrIntVF2,ArrIntVF2,ArrIntVF2, ArrVF2, ArrVF2,ArrTR,ArrVF2 )
 
   particlesByTimeStep = ParticlesByTimeStep(0.0,[0.0], 0.0, 0.0, 0.0) #Initial particlesByTimeStep
   particles = Particle[]  #Start array of particles empty
   advectingParticles = false
 
-   for currDay in range(startJulianDate, 1, endJulianDate-startJulianDate) #
+   for currDay in (startDate:Dates.Day(1):endDate) #
     #Verify we have some data in this day
-    if any(obj -> toJulianDate(obj.dates) == currDay, spillData)
+    if any(obj -> obj.dates == currDay, spillData)
       advectingParticles = true #We should start to reading vector fields and advectingParticles
       #Read from datos_derrame and init proper number of particles
       splitByTimeStep = SplitByTimeStep(particlesByTimeStep, spillData, modelConfigs, currDay)
 
     end
-      for currHour in range(0,deltaT,convert(Int64,24/deltaT))
-        currDate = DateTime(Dates.year(modelConfigs.startDate),12,31, currHour, 0, 0) + Dates.Day(currDay)
+      for currHour in Base.range(0,deltaT,convert(Int64,24/deltaT))
         if advectingParticles
-
           Particles = initParticles(particles, spillData, particlesByTimeStep, modelConfigs, currDay, currHour)
           if modelConfigs.model == "hycom"
             atmFilePrefix = "Dia_" #File prefix for the atmospheric netcdf files
@@ -31,19 +28,19 @@ function oilSpillModel(modelConfigs, FileName, ArrVF3, ArrVF2, ArrVF1,ArrIntVF2,
             println("CurrHour = ", currHour, " CurrDay = ", currDay)
             #Advecting particles
 
-            Particles = advectParticles2(VF, modelConfigs, particles, currDate)
+            Particles = advectParticles2(VF, modelConfigs, particles, currDay)
             #DegradingParticles
             Particles  = oilDegradation(particles, modelConfigs, spillData, particlesByTimeStep)
+            println(particles)
 
           elseif modelConfigs.model == "adcirc"
-
             atmFilePrefixADCIRC  = "fort.74." # File prefix for the atmospheric netcdf files
             oceanFilePrefixADCIRC  = "fort.64." # File prefix for the ocean netcdf files
             uvar = "u-vel"
             vvar = "v-vel"
             vF = vectorFieldsADCIRC(deltaT,currHour,currDay,VFADCIRC, modelConfigs, atmFilePrefixADCIRC, oceanFilePrefixADCIRC, uvar, vvar)
             println("CurrHour = ", currHour, " CurrDay = ", currDay )
-            Particles = advectParticlesADCIRC(VFADCIRC, modelConfigs, particles, currDate)
+            Particles = advectParticlesADCIRC(VFADCIRC, modelConfigs, particles, currDay)
 
 
           end
