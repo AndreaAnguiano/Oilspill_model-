394c1f4db8b7615708646e0bfb0a2895235ac8c1
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-07-25 18:57:01 -0500

    particlesByGroup

--- postProcessing/particlesByGroupAndType.jl
+++ postProcessing/particlesByGroupAndType.jl
@@ -5,18 +5,27 @@ pyplot()
 function particlesByGroup(particles::Array{Particle,1})
 
   allGroups = Array{String}(length(particles))
+  allTypes = Array{Int64}(length(particles))
 
   for particle in 1:length(particles)
     allGroups[particle] = particles[particle].status
+    allTypes[particle] = particles[particle].component
   end
   Groups = unique(allGroups)
-  totGroups = Array{Int64}(length(Groups))
+  totGroups = Array{Array{Int64}}(length(Groups))
+  Types = unique(allTypes)
 
   for group in 1:length(Groups)
-    totGroups[group] = length(find(x -> x == Groups[group], allGroups))
+    totGroups[group] = find(x -> x == Groups[group], allGroups)
   end
 
-
-  println(allGroups)
+  totGroupsAndTypes = Array{Array{Int64}}(length(Groups))
+  for group in totGroups
+    tempType = Array{Array{Int64}}(length(group))
+    for typ in Types
+      tempType[typ] = find(x -> x == Types[typ], group)
+    end
+    totGroupsAndTypes[group,:] = tempType
+  end
 
 end
