c5fc4971fef613f722a62e3a56e697a0a22b4bfb
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-07-28 16:40:04 -0500

    update math functions

--- processing/rotangle.jl
+++ processing/rotangle.jl
@@ -10,27 +10,27 @@ function rotangle(U,V)
 
   mu = 1.307*(1/10^6)
   g = 9.81
-  W = sqrt(U.^2+V.^2)
+  W = sqrt.(U.^2+V.^2)
   # c = mu * g
   # d_angle=25*exp(-(10^-8).*(W.^3/(mu*g)))
   d_angle = 34-7.5*(W).^(1/2)
   angled = d_angle*(2 * pi)/360
 
   #Create the complex vector of U and V
-  Z = complex(U, V)
+  Z = complex.(U, V)
 
   #Calculate the angle of complex vectors in degrees
-  ang = angle(Z)
+  ang = angle.(Z)
 
   #Magnitude of currents
-  r = abs(ang)
+  r = abs.(ang)
 
   #Now rotate all currents with Euler expression for complex numbers
-   complex_vectors = r.*exp(im*(ang - angled))
+   complex_vectors = r.*exp.(im*(ang - angled))
 
   #Components of rotated vectors
-  Ur = real(complex_vectors)
-  Vr = imag(complex_vectors)
+  Ur = real.(complex_vectors)
+  Vr = imag.(complex_vectors)
 
   [Ur, Vr]
 
