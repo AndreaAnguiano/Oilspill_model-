0a9dc4cbf15232caf4f1fb656f6de65be9e94d42
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-07-28 16:39:52 -0500

    update math functions

--- processing/interp.jl
+++ processing/interp.jl
@@ -13,6 +13,7 @@ function interp(TR, particle::Particle, VF::VectorFieldsADCIRC, VFvar)
   elseif d3[1]< threshold && d3[2] < threshold
     interp = VFvar[VF.ELE[TR[2],3]]
   else
+    println(size(VF.ELE), "  --", size(TR))
     interp = sum((VFvar[VF.ELE[TR[2],1]]/d1)
     + (VFvar[VF.ELE[TR[2],2]]/d2) + (VFvar[VF.ELE[TR[2],3]]/d3))/sum((1/d1) + (1/d2) + (1/d3))
   end
