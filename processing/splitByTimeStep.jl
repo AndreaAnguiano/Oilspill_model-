1487992c2657a0fa69f809e368fe459a47829d54
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-07-28 16:40:13 -0500

    update math functions

--- processing/splitByTimeStep.jl
+++ processing/splitByTimeStep.jl
@@ -4,10 +4,10 @@ function SplitByTimeStep(ParticlesByTimeStep, spillData, modelConfigs, Day)
   fractByTimeStep = modelConfigs.timeStep/24
   barrellsPerParticle = spillData[currDateIdx][1].barrellsPerParticle
 
-  ParticlesByTimeStep.particles = ceil(fractByTimeStep.*(spillData[currDateIdx][1].barrells[1]/barrellsPerParticle))
-  ParticlesByTimeStep.partSub = ceil(modelConfigs.subSurfaceFraction*fractByTimeStep.*(spillData[currDateIdx][1].barrells[2:end]./barrellsPerParticle))
-  ParticlesByTimeStep.burned = ceil(fractByTimeStep*(spillData[currDateIdx][1].burned/barrellsPerParticle))
-  ParticlesByTimeStep.evaporated = ceil(fractByTimeStep*(spillData[currDateIdx][1].evaporate/barrellsPerParticle))
-  ParticlesByTimeStep.recovered = ceil(fractByTimeStep*(spillData[currDateIdx][1].collected/barrellsPerParticle))
+  ParticlesByTimeStep.particles = ceil.(fractByTimeStep.*(spillData[currDateIdx][1].barrells[1]/barrellsPerParticle))
+  ParticlesByTimeStep.partSub = ceil.(modelConfigs.subSurfaceFraction*fractByTimeStep.*(spillData[currDateIdx][1].barrells[2:end]./barrellsPerParticle))
+  ParticlesByTimeStep.burned = ceil.(fractByTimeStep*(spillData[currDateIdx][1].burned/barrellsPerParticle))
+  ParticlesByTimeStep.evaporated = ceil.(fractByTimeStep*(spillData[currDateIdx][1].evaporate/barrellsPerParticle))
+  ParticlesByTimeStep.recovered = ceil.(fractByTimeStep*(spillData[currDateIdx][1].collected/barrellsPerParticle))
 
 end
