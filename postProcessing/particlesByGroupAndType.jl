37f0890cbb7474085c847cf40b22f9e1bfd88c3a
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-08-01 12:08:13 -0500

    particlesByGroupAndType

--- postProcessing/particlesByGroupAndType.jl
+++ postProcessing/particlesByGroupAndType.jl
@@ -1,5 +1,5 @@
 using Plots
-plotly()
+plotlyjs()
 #grupo en el caso del petroleo son evaporadas, quemadas, etc. Y tipo, son los componentes del petroleo.
 
 function particlesByGroup(particles::Array{Particle,1})
@@ -15,6 +15,7 @@ function particlesByGroup(particles::Array{Particle,1})
 
   GroupsAll =[length(find(x -> x == Groups[group],allGroups)) for group in 1:length(Groups)]
   GroupsAndTypes = zeros(Int64, length(Groups), length(Types))
+  TypesAll =[length(find(x -> x == Types[types], allTypes)) for types in 1:length(Types)]
 
   for group in 1:length(Groups)
     indxStatus = find(x-> x.status == Groups[group], particles)
@@ -25,9 +26,11 @@ function particlesByGroup(particles::Array{Particle,1})
       end
   end
 
-  newGroupsAndTypes = Array{Int64,1}[]
-  for group in 1:length(GroupsAndTypes[:,1])
-    push!(newGroupsAndTypes, GroupsAndTypes[group,:])
-  end
+  println(GroupsAndTypes)
+  groupedbar(Groups, GroupsAndTypes, legend= true, color_palette= :PuBu, xlabel= "Status", ylabel = "Particles", title = "Particles by status and oil type", ylims= maximum(GroupsAndTypes)+10)
+  #savefig("ParticlesByGroupAndOilType")
 
+  bar(["type 1", "type 2", "type 3", "type 4", "type 5", "type 6", "type 7", "type8"], TypesAll, xlabel = "Type of oil", ylabel = "Particles", title= "Particles by oil type", ylims= maximum(TypesAll)+200, legend=false)
+  #savefig("ParticlesByOilType")
+  gui()
 end
