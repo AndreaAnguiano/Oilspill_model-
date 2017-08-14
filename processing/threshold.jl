056cd8032687dc288c21ef4432ae401d18773d91
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-08-14 13:09:01 -0500

    threshold with types

--- processing/threshold.jl
+++ processing/threshold.jl
@@ -1,4 +1,4 @@
-function threshold(percentage, days_percent_decay, time_step)
+function threshold(percentage::Int64, days_percent_decay::Array{Int64,1}, time_step::Int64)
   time_percent_decay = days_percent_decay.*24./time_step
   thresholds = (1-percentage/100).^(1./time_percent_decay)
 end
