6fbc0cdf3cdfe7c8512d36bb1884ccc7beda0ff8
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-09-05 18:11:09 -0500

    splitByTimeStep

--- processing/splitByTimeStep.jl
+++ processing/splitByTimeStep.jl
@@ -3,7 +3,6 @@ function SplitByTimeStep(ParticlesByTimeStep, spillData, modelConfigs, Day)
   currDateIdx = find(obj -> obj.dates == Day, spillData)
   fractByTimeStep = modelConfigs.timeStep/24
   barrellsPerParticle = spillData[currDateIdx][1].barrellsPerParticle
-
   ParticlesByTimeStep.particles = ceil.(fractByTimeStep.*(spillData[currDateIdx][1].barrells[1]/barrellsPerParticle))
   ParticlesByTimeStep.partSub = ceil.(modelConfigs.subSurfaceFraction*fractByTimeStep.*(spillData[currDateIdx][1].barrells[2:end]./barrellsPerParticle))
   ParticlesByTimeStep.burned = ceil.(fractByTimeStep*(spillData[currDateIdx][1].burned/barrellsPerParticle))
