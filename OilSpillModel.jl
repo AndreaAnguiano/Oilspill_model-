14f56f21541fdee8bb3a217e5fc7e7ff7cb2565b
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-09-05 18:13:54 -0500

    oilspillmodel

--- OilSpillModel.jl
+++ OilSpillModel.jl
@@ -1,14 +1,21 @@
 
 
-function oilSpillModel(modelConfigs, FileName, ArrVF3, ArrVF2, ArrVF1,ArrIntVF2,ArrIntVF1, ArrTR, startDate, endDate, visualize, deltaT, lat, lon)
+function oilSpillModel(modelConfigs, FileName, ArrVF3, ArrVF2, ArrVF1,ArrIntVF2,ArrIntVF1, ArrTR,ArrDepthIndx, startDate, endDate, visualize, deltaT, lat, lon,VF3D)
   #Start of all fields with the initial conditions
   spillData = OilSpillData(FileName) #Reading all the values of the spill
-  VF = vectorFields(ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2, toJulianDate(startDate),0, lat, lon, ArrVF1,ArrVF1,ArrVF2,ArrVF2) #Initial vectorfield
-  VFADCIRC = VectorFieldsADCIRC(ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,toJulianDate(startDate),0,lat,lon, ArrVF1, ArrVF1, ArrVF2, "atmFilePrefix", "oceanFilePrefix", "uvar", "vvar", ArrIntVF2,ArrIntVF2,ArrIntVF2, ArrVF2, ArrVF2,ArrTR,ArrVF2 )
+
+  VF = vectorFields(ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF3,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,
+   toJulianDate(startDate),0, lat, lon, ArrVF1,ArrVF1,ArrDepthIndx,ArrVF2) #Initial vectorfield
+
+  VFADCIRC = VectorFieldsADCIRC(ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,ArrVF2,toJulianDate(startDate),0,lat,lon, ArrVF1, ArrVF1, ArrVF2,
+   "atmFilePrefix", "oceanFilePrefix", "uvar", "vvar", ArrIntVF2,ArrIntVF2,ArrIntVF2, ArrVF2, ArrVF2,ArrTR,ArrVF2 )
 
   particlesByTimeStep = ParticlesByTimeStep(0.0,[0.0], 0.0, 0.0, 0.0) #Initial particlesByTimeStep
   particles = Particle[]  #Start array of particles empty
   advectingParticles = false
+  if visualize
+    plotGulf()
+  end
 
    for currDay in (startDate:Dates.Day(1):endDate) #
     #Verify we have some data in this day
@@ -16,22 +23,29 @@ function oilSpillModel(modelConfigs, FileName, ArrVF3, ArrVF2, ArrVF1,ArrIntVF2,
       advectingParticles = true #We should start to reading vector fields and advectingParticles
       #Read from datos_derrame and init proper number of particles
       splitByTimeStep = SplitByTimeStep(particlesByTimeStep, spillData, modelConfigs, currDay)
-
-    end
+    end 
       for currHour in range(0,deltaT,convert(Int64,24/deltaT))
         if advectingParticles
           Particles = initParticles(particles, spillData, particlesByTimeStep, modelConfigs, currDay, currHour)
           if modelConfigs.model == "hycom"
             atmFilePrefix = "Dia_" #File prefix for the atmospheric netcdf files
             oceanFilePrefix = "archv.2010_" #File prefix for the ocean netcdf files
+            if VF3D
+              vF = VectorFields3D(deltaT,currHour,currDay, VF, modelConfigs, atmFilePrefix, oceanFilePrefix)
+              println("CurrHour = ", currHour, " CurrDay = ", currDay)
+              #Advecting particles
+              Particles = advectParticles3D(VF, modelConfigs, particles, currDay)
+              #DegradingParticles
+              Particles  = oilDegradation(particles, modelConfigs, spillData, particlesByTimeStep)
 
-            vF = VectorFields2(deltaT,currHour,currDay, VF, modelConfigs, atmFilePrefix, oceanFilePrefix)
-            println("CurrHour = ", currHour, " CurrDay = ", currDay)
-            #Advecting particles
-
-            Particles = advectParticles2(VF, modelConfigs, particles, currDay)
-            #DegradingParticles
-            Particles  = oilDegradation(particles, modelConfigs, spillData, particlesByTimeStep)
+            else
+              vF = VectorFields2(deltaT,currHour,currDay, VF, modelConfigs, atmFilePrefix, oceanFilePrefix)
+              println("CurrHour = ", currHour, " CurrDay = ", currDay)
+              #Advecting particles
+              Particles = advectParticles2(VF, modelConfigs, particles, currDay)
+              #DegradingParticles
+              Particles  = oilDegradation(particles, modelConfigs, spillData, particlesByTimeStep)
+            end
 
           elseif modelConfigs.model == "adcirc"
             atmFilePrefixADCIRC  = "fort.74." # File prefix for the atmospheric netcdf files
@@ -52,7 +66,7 @@ function oilSpillModel(modelConfigs, FileName, ArrVF3, ArrVF2, ArrVF1,ArrIntVF2,
           lastLat = [particles[ind].lat[end] for ind in LiveParticlesIndx]
           lastLon = [particles[ind].lon[end] for ind in LiveParticlesIndx]
           lastDepth = [particles[ind].depths[end] for ind in LiveParticlesIndx]
-          plotParticles(lastLon, lastLat, lastDepth)
+          plotParticles2D(lastLon, lastLat)
           gui()
 
         end
