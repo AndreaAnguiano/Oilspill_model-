efd84c24d10cd756cce435eac30d99f1cc1614b7
AndreaAnguiano <andrea_anguiano@ciencias.unam.mx>
2017-09-06 12:42:05 -0500

    vector fields 3D

--- readData/VectorFields3D.jl
+++ readData/VectorFields3D.jl
@@ -1,5 +1,5 @@
 using NetCDF
-function VectorFields2(deltaT,currHour, currDay, VF, modelConfig, atmFilePrefix, oceanFilePrefix)
+function VectorFields3D(deltaT,currHour, currDay, VF, modelConfig, atmFilePrefix, oceanFilePrefix)
   path = "/media/petroleo/Datos/"
   windDeltaT = 6
   myEps = .01
@@ -80,14 +80,97 @@ function VectorFields2(deltaT,currHour, currDay, VF, modelConfig, atmFilePrefix,
   end
 
   #----------------- reading data -------------------------
+
+  rangetest = range(VF.depthsMinMax[1], VF.depthsMinMax[2]-VF.depthsMinMax[1]+1)
+  cutIndx = zeros(Int64,length(unique(VF.depthsIndx)))
+  for indx in range(1,length(unique(VF.depthsIndx)))
+    cutIndx[indx] = find(x-> x == VF.depthsIndx[indx], rangetest)[1]
+  end
+
   #verify if we need to read the winds and currents for the current day
   if firstRead
     #reading currents for current day
-    T1 = ncread(path*readOceanFile, "U", [1, 1, VF.depthsMinMax[1]], [-1,-1, VF.depthsMinMax[2]])
-    T2 = ncread(path*readOceanFile, "V", [1, 1, VF.depthsMinMax[1]], [-1,-1, VF.depthsMinMax[2]])
+    T1 = ncread(path*readOceanFile, "U", [1, 1, VF.depthsMinMax[1]], [-1,-1,VF.depthsMinMax[2]-VF.depthsMinMax[1]+1])
+    T2 = ncread(path*readOceanFile, "V", [1, 1, VF.depthsMinMax[1]], [-1,-1, VF.depthsMinMax[2]-VF.depthsMinMax[1]+1])
     #Cut U and V to the only depth levels that we are going to use
-    VF.UD = flipdim(rotr903D(T1[:,:,unique(VF.depthsIndx)],1),3)
-    VF.VD = flipdim(rotr903D(T2[:,:,unique(VF.depthsIndx)],1),3)
+    VF.UD = flipdim(rotr903D(T1[:,:,cutIndx],1),3)
+    VF.VD = flipdim(rotr903D(T2[:,:,cutIndx],1),3)
     #reading winds for current day
 
+    TempUW = ncread(path*readWindFile, "U_Viento")
+    TempVW = ncread(path*readWindFile, "V_Viento")
+    VF.UWRD, VF.VWRD = rotangle(TempUW', TempVW')
+  end
+  #Verify if we need to read the winds for the next day
+  if readWindT2
+    if !firstRead
+      VF.UWRD = VF.UWRDT2
+      VF.VWRD = VF.VWRDT2
+    end
+    TempUW = ncread(path*readWindFileT2, "U_Viento")
+    TempVW = ncread(path*readWindFileT2, "V_Viento")
+    #Obtain the next rotated winds
+    VF.UWRDT2, VF.VWRDT2 = rotangle(TempUW',TempVW')
+
+    #Update the temporal variable that holds U(d_1) - U(d_0) used for the interpolation
+
+    VF.UWRDT2minusUWRDT = VF.UWRDT2 - VF.UWRD
+    VF.VWRDT2minusVWRDT = VF.VWRDT2 - VF.VWRD
+  end
+
+  #making the interpolation to the proper 'timesteps'
+  if readWindT2
+    if firstRead
+      #The final U and V (winds) corresponds, in this case to the current timeframe values
+      VF.UWR = VF.UWRD
+      VF.VWR = VF.VWRD
+    else
+      #The final U and V (winds) corresponds, in this case, to the previous timeframe values
+      VF.UWR = VF.UWRT2
+      VF.VWR = VF.VWRT2
+    end
+    #Interpolate UWT2 correctly, depending on the model hour
+    VF.UWRT2 = VF.UWRD + (((currHour%windDeltaT)+ modelConfig.timeStep)/windDeltaT)*VF.UWRDT2minusUWRDT
+    VF.VWRT2 = VF.VWRD + (((currHour%windDeltaT)+ modelConfig.timeStep)/windDeltaT)*VF.VWRDT2minusVWRDT
+  else
+    #The final U and V (winds) corresponds, in this case, to the current timeframe values
+    VF.UWR = VF.UWRT2
+    VF.VWR = VF.VWRT2
+    #Interpolate UWT2 correctly, depending on the model hour
+    VF.UWRT2 = VF.UWRD + (((currHour%windDeltaT)+ modelConfig.timeStep)/windDeltaT)*VF.UWRDT2minusUWRDT
+    VF.VWRT2 = VF.VWRD + (((currHour%windDeltaT)+ modelConfig.timeStep)/windDeltaT)*VF.VWRDT2minusVWRDT
+  end
+
+  #------------------ reading and organizing the currents------------------------------
+  if readOceanT2
+    T1 = ncread(path*readOceanFileT2, "U",[1, 1, VF.depthsMinMax[1]], [-1,-1,VF.depthsMinMax[2]-VF.depthsMinMax[1]+1])
+    T2 = ncread(path*readOceanFileT2, "V", [1, 1, VF.depthsMinMax[1]], [-1,-1, VF.depthsMinMax[2]-VF.depthsMinMax[1]+1])
+    #Cut U and V to the only depth levels that we are going to use
+
+    VF.UDT2 = flipdim(rotr903D(T1[:,:,cutIndx],1),3)
+    VF.VDT2 = flipdim(rotr903D(T2[:,:,cutIndx],1),3)
+  end
+
+  #making the interpolation to the proper timesteps
+  if currHour == 0
+    VF.U = VF.UD
+    VF.V = VF.VD
+    #Update the temporal variable that holds U(d_1) - U(d_0)
+    VF.UDT2minusUDT = VF.UDT2 - VF.UD
+    VF.VDT2minusVDT = VF.VDT2 - VF.VD
+    #interpolate UT2 correctly, depending on the model hour
+    VF.UT2 = VF.UD + ((currHour+modelConfig.timeStep)/24)*VF.UDT2minusUDT
+    VF.VT2 = VF.VD + ((currHour+modelConfig.timeStep)/24)*VF.VDT2minusVDT
+  else
+    VF.U = VF.UT2
+    VF.V = VF.VT2
+    #Interpolate UT2 and VT2 correctly, depending on the model hour
+
+    VF.UT2 = VF.UD + ((currHour+modelConfig.timeStep)/24)*VF.UDT2minusUDT
+    VF.VT2 = VF.VD + ((currHour+modelConfig.timeStep)/24)*VF.VDT2minusVDT
+  end
+
+  #Update the current time that has already been executed (read in this case)
+  VF.currDay = currDay
+  VF.currHour = currHour
 end
