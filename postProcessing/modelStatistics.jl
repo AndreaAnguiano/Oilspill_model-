ddf0c4b046f783a48df52af26224b42daac5868d
Andrea Isabel Anguiano García <andrea_anguiano@ciencias.unam.mx>
2018-02-12 10:37:28 -0600

    functions that generate the statistics

--- postProcessing/modelStatistics.jl
+++ postProcessing/modelStatistics.jl
@@ -1,1 +1,11 @@
-function modelStatistics(Particles::Array{Particles,1})
+using Plots
+plotlyjs()
+
+function modelStatistics(particles::Array{Particle,1}, positions::Array{Float64,2},ratio::Float64)
+  particlesByLocation(particles, positions,ratio)
+
+  particlesByTypeAndDate(particles)
+
+  particlesByGroup(particles)
+
+end
