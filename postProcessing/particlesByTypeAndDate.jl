ba8fc1beac1ceaaabee094a631329eeaa3259864
Andrea Isabel Anguiano García <andrea_anguiano@ciencias.unam.mx>
2018-02-12 10:51:17 -0600

    changing the algorithm for the average life of the particles

--- postProcessing/particlesByTypeAndDate.jl
+++ postProcessing/particlesByTypeAndDate.jl
@@ -1,7 +1,7 @@
 using Plots
 plotlyjs()
 
-function particlesByTypeAndDate(particles::Array{Particle,1})
+function particlesByTypeAndDate(particles::Array{Particle,1},timeStep::Int64)
   allTypes = Array{Int64}(length(particles))
 
   for particle in 1:length(particles)
@@ -13,8 +13,12 @@ function particlesByTypeAndDate(particles::Array{Particle,1})
   for types in 1:length(Types)
     indxTypes = find(x-> x.component == Types[types], particles)
     subPart = particles[indxTypes]
-    TypeAndDates[types] = mean([x.currTimeStep for x in subPart])
+    #print("types: ", types, " mean: ",[x.currTimeStep for x in subPart])
+    TypeAndDates[types] = mean([x.currTimeStep - 1 for x in subPart])/(24/timeStep)
   end
   typesString = ["Type 1", "Type 2", "Type 3", "Type 4", "Type 5", "Type 6", "Type 7", "Type 8"]
   plotByTypeAndDate(typesString, TypeAndDates)
+  gui()
+  savefig("plotByTypeAndDate")
+
 end
