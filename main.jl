f936fa11d2484f935072381a8017e22d2bf0d377
Andrea Isabel Anguiano García <andrea_anguiano@ciencias.unam.mx>
2018-02-12 11:24:16 -0600

    adding changes for multiple oil spills

--- main.jl
+++ main.jl
@@ -1,16 +1,17 @@
-
+pyplot()
 #----initial conditions----
 function main()
-  Days = 20
-  startDate = DateTime(2010,04,25)
+  Days = 3
+
+  startDate = DateTime(2010,04,30)
   endDate =startDate + Dates.Day(Days)
-  depths = [0, 800,1000]
+  depths = [0, 400,1000]
   components = [0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.3; 0.0 0.0 0.0 0.1 0.1 0.1 0.2 0.5; 0.0 0.0 0.0 0.0 0.0 0.1 0.2 0.7; ]
   subSurfaceFraction = [1/3, 1/3, 1/3]
   initPartSize = 10
   totComponents = 8
   windContrib = 0.035
-  turbulentDiff = 1.0
+  turbulentDiff = 0.2
   diffusion = 0.005
   evaporate = [1]
   biodeg = [1]
@@ -18,9 +19,10 @@ function main()
   collected = [1]
   timeStep = 12
   byComponent = threshold(95,[4, 8, 12, 16, 20, 24, 28, 32], timeStep)
-  FileName = "/media/petroleo/Datos/datos_derrame.csv"
+  barrellsPerParticle = 100
+
   ArrVF1 = zeros(3)
-  ArrVF3 = zeros(3,3,3) # Initial array for vectorFields 3x3
+    ArrVF3 = zeros(3,3,3) # Initial array for vectorFields 3x3
   ArrVF2 = zeros(3,2) # Initial array for vectorFields 3x2
   ArrIntVF2 = ones(Int64,1,1)
   ArrIntVF1 = ones(Int64,1)
@@ -31,17 +33,38 @@ function main()
   decay = Decay(burned,collected, evaporate, biodeg, byComponent)
   lat = [28.0]
   lon = [-88.0]
-  visualize = true
+
+  visualize = false
   model = "hycom"
   VF3D = false
+  positions = [[28, -88], [20, -95]]
+  spillType = "oil"
+  Statistics = false
 
-  modelConfigs = modelConfig(startDate,endDate, depths, lat, lon, components, subSurfaceFraction, decay, timeStep, initPartSize, totComponents, windContrib, turbulentDiff, diffusion, model)
+  lims = [-97 -80; 20 31]
+  if spillType == "simple" #multiple oil spills
+    FileName = "/home/andrea/Data/Datos/ndatos_derrame.csv"
+    lims = [-98 -80; 18 31]
+  else #one oil spill from Oil Budget Calculator (2010)
+    FileName = "/home/andrea/Data/Datos/spill_data.csv"
+  end
 
-  particles = oilSpillModel(modelConfigs, FileName, ArrVF3, ArrVF2, ArrVF1,ArrIntVF2,ArrIntVF1,ArrTR, ArrDepthIndx, startDate, endDate, visualize, timeStep, lat, lon, VF3D)
-  #particlesByLocation(particles, [28.0 -88.0; 38.0 -88.0; 37.8 -88], 10.0)
-  #particlesByGroup(particles)
+  modelConfigs = modelConfig(startDate,endDate, depths, components, subSurfaceFraction, decay, timeStep, initPartSize, totComponents, windContrib, turbulentDiff, diffusion, model, spillType)
+  particles = oilSpillModel(modelConfigs, FileName, ArrVF3, ArrVF2, ArrVF1,ArrIntVF2,ArrIntVF1,ArrTR, ArrDepthIndx, startDate, endDate, visualize, timeStep, lat, lon, VF3D, positions, barrellsPerParticle, lims)
 
+  if Statistics
+    modelStatistics(particles,[28.0 -88.0; 38.0 -88.0; 37.8 -88], 10.0 )
+  end
+  #particlesByLocation(particles,[28.0 -88.0; 38.0 -88.0; 37.8 -88], 10.0 )
+  #particlesByTypeAndDate(particles,12)
+  particlesByGroup(particles)
+  #print(particles)
 
+  # funciones para guardar posiciones de partículas
+  #lat = [i.lat for i in particles]
+  #lon = [i.lon for i in particles]
+  #writedlm("particleslats$days30.txt", lat)
+  #writedlm("particleslons$days.txt", lon)
 
 end
 main()
@@ -51,12 +74,3 @@ main()
 #using ProfileView
 #ProfileView.view()
 #@time main()
-
-
-
-
-
-
-
-
-#
